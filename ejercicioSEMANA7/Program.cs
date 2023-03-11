efe7aab6059ea781977d36523342e4075f376828
Melvin1perez <125324725+Melvin1perez@users.noreply.github.com>
2023-03-10 19:09:57 -0600

    Add files via upload

---
+++ ejercicioSEMANA7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using System;
+
+namespace Triangulo
+{
+    class Program
+    {
+        static void Main(string[] arg)
+        {
+            Console.Write("Introduce un número entero: ");
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
